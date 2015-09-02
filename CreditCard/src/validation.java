cd92d7fb9b122a82e8fd7753e1c6df3239a864f5
Classified56 <davis.ranney@MU-C1-01.mullen.pvt.k12.co.us>
2015-09-02 14:19:07 -0600

    First save.

---
+++ CreditCard/src/validation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class validation
+	{
+		static Scanner userInput = new Scanner(System.in);
+		public static void main(String[] args)
+			{
+				System.out.print("Please input your possible credit card number. ");
+			}
+
+	}
