3501287d126b9aecabfef26b5aa777fa89c4d43c
Classified56 <davis.ranney@MU-C1-01.mullen.pvt.k12.co.us>
2015-09-02 14:57:42 -0600

    COMPLETED IN LESS THAN AN HOUR

--- CreditCard/src/validation.java
+++ CreditCard/src/validation.java
@@ -4,7 +4,39 @@ public class validation
 		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-				System.out.print("Please input your possible credit card number. ");
+				System.out.print("Please input your possible credit card number. (no spaces) ");
+				long cardNum = userInput.nextLong();
+				long cardArray[] = new long[16];
+				for(int i = 0; i < 16; i++)
+					{
+						if((i % 2) == 1)
+							{
+								if(((cardNum % 10) * 2) > 9)
+									{
+										long temp = (cardNum % 10) * 2;
+										long second = temp % 10;
+										long first = temp / 10;
+										cardArray[15 - i] = first + second;
+									}
+								else
+									{
+										cardArray[15 - i] = (cardNum % 10) * 2;
+									}
+							}
+						else
+							{
+								cardArray[15 - i] = (cardNum % 10);
+							}
+						cardNum /= 10;
+					}
+				long sum = 0;
+				for(long steve: cardArray)
+					{
+						sum += steve;
+					}
+				if(sum % 10 == 0)
+					System.out.println("You have a valid card number. ");
+				else
+					System.out.println("That card number is invalid. ");
 			}
-
 	}
